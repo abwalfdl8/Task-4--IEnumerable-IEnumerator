5d6336349cf96ddb2acaa7b9e75c2aa8f0ab8490
Tweety <Tweety@DESKTOP-L5VOLC2>
2026-01-02 21:36:46 +0200

    Add project files.

---
+++ Odd Number Iterator/MyList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odd_Number_Iterator
+{
+    internal class MyList :IEnumerable
+    {
+        int[] arr;
+        int CurrentIndex;
+        public MyList()
+        {
+            arr = new int[5];
+            CurrentIndex = -1;
+        }
+
+        public int Capacity()
+        {
+            return arr.Length;
+        }
+
+        public int Count()
+        {
+            return CurrentIndex + 1;
+        }
+
+        public void Add(int Num)
+        {
+           if(CurrentIndex == arr.Length-1) 
+           {
+             Extend();
+           }
+            arr[++CurrentIndex] = Num;
+        }
+        private void Extend()
+        {
+            int[] newArr = new int[arr.Length*2];
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        }
+
+        public int GetByIndex(int index)
+        {
+            if(index<0 || index>= Capacity())
+            {
+                Console.WriteLine("Index is out of range.");
+            }
+            return arr[index];
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new Iterator(this);
+        }
+
+        class Iterator : IEnumerator
+        {
+            MyList mylist;
+            int index;
+            public Iterator(MyList mylist)
+            {
+               this.mylist = mylist;
+                index = -1;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    return mylist.arr[index];
+                }
+            }
+            public bool MoveNext()
+            {
+              
+               while(++index <= mylist.CurrentIndex)
+                {
+                    if(mylist.arr[index]%2 != 0)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Odd Number Iterator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Odd_Number_Iterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList mylist = new MyList() {1,2,3,4,5,6,7,8,9,10};
+
+            foreach (int n in mylist)
+            {
+              Console.WriteLine(n);
+            }
+
+
+
+
+        }
+    }
+}
